e66a50678d25cdfb30696ad7283569256f0dbc9e
SATYAPRAKASH <satyaprakashswain37017@gmail.com>
2025-07-04 11:21:41 +0530

    added cors to backend

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import express, { Request, Response } from "express"
 import rootRouter from './routes/rootRouter'
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import cors from "cors"
 dotenv.config();
 const app = express()
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+}))
 app.use(express.json());
 app.use(cookieParser());
 const PORT=process.env.PORT||8080;
